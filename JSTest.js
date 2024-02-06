2cc4879ab20f4b1d9d8d2c0d9bdde7793a2be828
Merl <Merlince@host.com>
2024-02-07 00:35:57 +0700

    add more api route

--- JSTest.js
+++ JSTest.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
+
+
+
+
+
 
 var connection = mysql.createConnection({
     host: '127.0.0.1',
@@ -16,11 +22,23 @@ connection.connect(function(err) {
 
 
 
-let app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const app = express();
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+app.get('/' , (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'html_lake/Docs.html'));
+});
+
 
 app.get('/getFaculty', (req, res) => {
     connection.query(`SELECT * FROM faculty`, function(err, results) {
@@ -29,7 +47,23 @@ app.get('/getFaculty', (req, res) => {
 });
 
 app.get('/getFaculty/:Fact_En', (req, res) => {
-    connection.query(`SELECT * FROM faculty WHERE Fact_En = '${req.params.Fact_En}'`, function(err, results) {
+    let fact = req.params.Fact_En.toLowerCase();
+    connection.query(`SELECT * FROM faculty WHERE Fact_En = '${fact}'`, function(err, results) {
         res.send(results);
     });
 });
+app.get('/getStatus', (req, res) => {
+    connection.query(`SELECT * FROM reserve_status`, function(err, results) {
+        res.send(results);
+    });
+});
+app.get('/getAllTable', (req, res) => {
+    connection.query(`Show tables`, function(err, results) {
+        res.send(results);
+    });
+});
+app.post('/CustomerRegis',express.json,(req,res,next) => {
+
+});
+
+
